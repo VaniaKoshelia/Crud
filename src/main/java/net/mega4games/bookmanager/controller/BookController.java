9707af63b91f1d9771c2c0c89e64bcdac79114de
VaniaKoshelia <36624074+VaniaKoshelia@users.noreply.github.com>
2018-02-19 15:35:33 +0300

    Add files via upload

---
+++ src/main/java/net/mega4games/bookmanager/controller/BookController.java
@@ -0,0 +1,80 @@
+package net.mega4games.bookmanager.controller;
+
+import net.mega4games.bookmanager.model.Book;
+import net.mega4games.bookmanager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+    private BookService bookService;
+
+    @Autowired(required = true)
+    @Qualifier(value = "bookService")
+    public void setBookService(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping(value = "books", method = RequestMethod.GET)
+    public String listBooks(Model model){
+        model.addAttribute("book", new Book());
+        model.addAttribute("listBooks", this.bookService.listBooks());
+
+        return "books";
+    }
+
+    @RequestMapping(value = "/books/add", method = RequestMethod.POST)
+    public String addBook(@ModelAttribute("book") Book book){
+        if(book.getId() == 0){
+            this.bookService.addBook(book);
+        }else {
+            this.bookService.updateBook(book);
+        }
+
+        return "redirect:/books";
+    }
+
+    @RequestMapping("/remove/{id}")
+    public String removeBook(@PathVariable("id") int id){
+        this.bookService.removeBook(id);
+
+        return "redirect:/books";
+    }
+
+    @RequestMapping("edit/{id}")
+    public String editBook(@PathVariable("id") int id, Model model){
+        model.addAttribute("book", this.bookService.getBookById(id));
+        model.addAttribute("listBooks", this.bookService.listBooks());
+
+        return "books";
+    }
+
+    @RequestMapping("bookdata/{id}")
+    public String bookData(@PathVariable("id") int id, Model model){
+        model.addAttribute("book", this.bookService.getBookById(id));
+
+        return "bookdata";
+    }
+
+    @RequestMapping("read/{id}")
+    public String readBook(@PathVariable("id")int id) {
+        this.bookService.readBook(id);
+        return "redirect:/books";
+    }
+
+    @RequestMapping(value = "books/search", method = RequestMethod.POST)
+    public String searchBook(@ModelAttribute("book") Book book, Model model) {
+        Book book_id = this.bookService.searchBook(book);
+        if(book_id.getId() != 0){
+            return bookData(book_id.getId(), model);
+        }else {
+            return "redirect:/books";
+        }
+    }
+}
